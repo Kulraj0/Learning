eb144bc943a1b4154f5cc0814183ba90dfbf10f1
Kulraj0 <Kulraj1081@gmail.com>
2024-10-28 14:18:24 +0530

    Update number_guess.py

--- py/number_guess.py
+++ py/number_guess.py
@@ -1,6 +1,41 @@
 import random
 
-range = input("Input a number for the range in which you want to play")
+range = input("Input a number for the range in which you want to play \n")
 
-number = random(0,range)
-guess = input()
+if range.isdigit():
+    range = int(range)
+    if range<0:
+        print("Number should be greater than 0")
+        quit()
+else:
+    print("Enter a valid number")
+    quit()
+
+number = random.randint(1, range + 1)
+
+tries = 0
+
+while True:
+    guess = input(f"Guess the number between 1 and {range} \n")
+    if guess.isdigit():
+        guess = int(guess)
+        tries += 1
+        if guess == number:
+            print("You got it! in ", tries, "tries")
+            continue
+        else:
+            print("you got it incorrect,try again")
+            if guess>number:
+                if guess>number/2:
+                    print("Your guess is too great than number")
+                else:
+                    print("your guess is greater than number")
+            else:
+                if guess<number/2:
+                    print("Your guess is too lower than number")
+                else:
+                    print("your guess is lower than number")
+            continue
+            
+    else:
+        print("Enter a valid number")
