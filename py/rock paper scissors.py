002e16ac5c8127fe8556e00cb759e4f274b80cc0
Kulraj0 <Kulraj1081@gmail.com>
2024-10-29 05:59:49 +0530

    Update rock paper scissors.py

--- py/rock paper scissors.py
+++ py/rock paper scissors.py
@@ -2,35 +2,38 @@ import random
 
 Options = ["rock", "paper", "scissors"]
 
-print("Game begins! \n To stop playing enter Q anytime you want \n What do you choose")
+print("Game begins! \n To stop playing enter Q anytime you want \n ")
+
+user_wins = 0
+comp_wins = 0
 
 while True:
-    user_guess = input("What do you choose")
+    user_guess = input("What do you choose \n")
     user_guess = user_guess.lower()
-    if user_guess not in Options:
-        print("Enter a valid input")
-
-    comp_guess = Options[random.randint(0,2)]
-
-    user_wins = 0
-    comp_wins = 0
 
     if user_guess == "q":
         print(f"You won {user_wins} times and Computer won {comp_wins} times")
         quit()
-    elif user_guess == "rock" and comp_guess == "scissors" :
-        print(f"Computer chose {comp_guess} \n You win!")
+    elif user_guess not in Options:
+        print("Enter a valid input")
+        continue
+
+    comp_guess = Options[random.randint(0,2)]
+
+    
+    if user_guess == "rock" and comp_guess == "scissors" :
+        print(f"Computer chose {comp_guess} \nYou win!")
         user_wins += 1
     elif user_guess == "paper" and comp_guess == "rock" :
-        print(f"Computer chose {comp_guess} \n You win!")
+        print(f"Computer chose {comp_guess} \nYou win!")
         user_wins += 1
     elif user_guess == "scissors" and comp_guess == "paper" :
-        print(f"Computer chose {comp_guess} \n You win!")
+        print(f"Computer chose {comp_guess} \nYou win!")
         user_wins += 1
     elif user_guess == comp_guess:
         print("Its a draw")
     else:
-        print(f"Computer chose{comp_guess} \n You lost!")
+        print(f"Computer chose{comp_guess}\nYou lost!")
         comp_wins += 1
 
     
